0baaef54c55e786a13515234d85ce7d9bc2077a4
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-11-08 21:32:11 -0300

    Add date limit in inputs

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -6,7 +6,12 @@ export default function DateInput(props) {
 
 	return (
 		<StyleDateFilter>
-			<input type="date" onChange={onChange} />
+			<input
+				type="date"
+				onChange={onChange}
+				min="2008-09-28"
+				max="2018-08-07"
+			/>
 			<p>{label}</p>
 		</StyleDateFilter>
 	);
